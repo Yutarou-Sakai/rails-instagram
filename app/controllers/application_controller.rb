34a97c15b35265312ec7a278053e66895816050f
酒井友太郎 <>
2021-07-26 16:18:26 +0900

    deviseでログイン機能を追加,(usernameあり)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+    protect_from_forgery with: :exception #CSRF対策
+    before_action :configure_permitted_parameters, if: :devise_controller?
+
+    protected
+
+    def configure_permitted_parameters # usernameとpasswordでログインできるようにする
+        added_attrs = [ :email, :username, :password, :password_confirmation ] #許可する情報
+        devise_parameter_sanitizer.permit :sign_up, keys: added_attrs #sign upで許可
+        devise_parameter_sanitizer.permit :account_update, keys: added_attrs #updateで許可
+        devise_parameter_sanitizer.permit :sign_in, keys: added_attrs #sign inで許可
+    end
 end

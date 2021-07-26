e3e4e7ab9a4dcff0eded1bd227c14d86a98eb1b5
酒井友太郎 <>
2021-07-26 16:56:33 +0900

    usernameでログインできるように変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,22 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable,
+         :authentication_keys => [:login]
+
+  attr_accessor :login
+
+  validates :username,
+    uniqueness: { case_sensitive: :false }, #一意のusernameで大小文字を区別しない
+    length: { minimum: 3, maximum: 20 } #3文字以上20文字以内
+
+
+  def self.find_first_by_auth_conditions(warden_conditions)
+    conditions = warden_conditions.dup
+    if login = conditions.delete(:login)
+      where(conditions).where(["username = :value OR lower(email) = lower(:value)", { :value => login }]).first
+    else
+      where(conditions).first
+    end
+  end
 end

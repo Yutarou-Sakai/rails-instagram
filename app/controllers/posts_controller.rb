6a09c21011d65a3af366a25898131806e7d622d1
酒井友太郎 <>
2021-07-31 19:48:08 +0900

    postの一覧と新規作成をcontrollerに追加

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,31 @@
 class PostsController < ApplicationController
-    index do
+    before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
 
+    def index
+        @posts = Post.all
+    end 
+
+    def new
+        @post = current_user.posts.build
+    end
+
+    def create
+        @post = current_user.posts.build(post_params)
+        if @post.save
+            redirect_to post_path(@post), notice: '保存ができました'
+        else
+            flash.now[:error] = '保存できませんでした'
+            render :new
+        end
+    end
+
+
+    private
+    def post_params #フォームの入力内容
+        params.require(:post).permit(:content, :content_images)
+    end
+
+    def set_article
+        @post = Post.find(params[:id])
     end
 end

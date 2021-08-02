6a1334e5a8866a47ccf48ee1f71b9bc27ce1199b
酒井友太郎 <>
2021-08-02 19:44:51 +0900

    likeのcreate・destroyアクションを追加

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,2 +1,22 @@
 class LikesController < ApplicationController
+    before_action :authenticate_user!
+    before_action :set_post
+
+    def create
+        post.likes.create!(user_id: current_user.id)
+
+        render json: { status: 'ok' }
+    end
+
+    def destroy
+        like =  post.likes.find_by!(user_id: current_user.id)
+        like.destroy!
+
+        render json: { status: 'ok' }
+    end
+
+    private
+    def set_post
+        post = Post.find(params[:post_id])
+    end
 end

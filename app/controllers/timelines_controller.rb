90f115bd372ef2a3b3e6cb59dc0f520daca0e054
酒井友太郎 <>
2021-08-11 16:32:52 +0900

    timelineを24時間以内の投稿でいいねが多いものを5つ表示する設定に変更

--- app/controllers/timelines_controller.rb
+++ app/controllers/timelines_controller.rb
@@ -3,6 +3,13 @@ class TimelinesController < ApplicationController
 
     def show
         user_ids = current_user.followings.pluck(:id)
-        @posts = Post.where(user_id: user_ids)
+
+        from  = Time.current.at_beginning_of_day
+        to    = (from + 1.day)
+
+        @popular_posts = Post.joins(:likes).group("posts.id").order("count(posts.id) desc").limit(5)
+
+        @posts = @popular_posts.where(created_at: from...to)
+
     end
 end

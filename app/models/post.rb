1c49e45f65718338fad09faaf1b25cc2421b33e6
酒井友太郎 <>
2021-08-12 18:22:02 +0900

    投稿のバリデーションを追加

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,4 +5,38 @@ class Post < ApplicationRecord
   has_many :comments, dependent: :destroy #投稿は複数のコメントを持つ
 
   has_many_attached :content_images #active strageで画像を複数枚保存
+
+  validates :content, presence: true
+  validates :content, length: { minimum: 2, maximum: 100 }
+
+  validates :content_images, presence: true
+  validate :content_image_type, :content_image_size, :content_image_length
+
+
+  private
+
+  def content_image_type
+    content_images.each do |content_image|
+      if !content_image.blob.content_type.in?(%('image/jpeg image/png'))
+        content_image.purge
+        errors.add(:content_images, 'はjpegまたはpng形式でアップロードしてください')
+      end
+    end
+  end
+
+  def content_image_size
+    content_images.each do |content_image|
+      if content_image.blob.byte_size > 5.megabytes
+        content_image.purge
+        errors.add(:content_images, "は1つのファイル5MB以内にしてください")
+      end
+    end
+  end
+
+  def content_image_length
+    if content_images.length > 4
+      content_images = nil
+      errors.add(:content_images, "は4枚以内にしてください")
+    end
+  end
 end

eb12f25d0bc5d5d4e199f6a7d724b07a00ace922
酒井友太郎 <>
2021-08-13 22:17:46 +0900

    test model-spec 画像と本文が入力されている場合、投稿を保存できる

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,4 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
+  it '画像と本文が入力されている場合、投稿を保存できる' do
+    user = User.create!({
+      email: 'test@test.com',
+      username: 'test',
+      password: '123456'
+    })
+
+    post = user.posts.build({
+      content: Faker::Lorem.characters(number: 10),
+    })
+    post.content_images.attach(io: File.open('app/assets/images/test1.jpg'), filename: 'test1.jpg')
+
+    expect(post).to be_valid
+  end
 end

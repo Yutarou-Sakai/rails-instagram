219d927b69c137d078625f8290049c6d9106aaec
酒井友太郎 <>
2021-08-13 22:49:46 +0900

    test model spec 画像が5枚以上の場合、投稿を保存できない

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,18 +1,45 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  it '画像と本文が入力されている場合、投稿を保存できる' do
-    user = User.create!({
-      email: 'test@test.com',
-      username: 'test',
-      password: '123456'
-    })
+  context '画像と本文が入力されている場合' do
+    before do
+      user = User.create!({
+        email: 'test@test.com',
+        username: 'test',
+        password: '123456'
+      })
+      @post = user.posts.build({
+        content: Faker::Lorem.characters(number: 10),
+      })
+      @post.content_images.attach(io: File.open('app/assets/images/test1.jpg'), filename: 'test1.jpg')
+      @post.content_images.attach(io: File.open('app/assets/images/test1.jpg'), filename: 'test1.jpg')
+    end
 
-    post = user.posts.build({
-      content: Faker::Lorem.characters(number: 10),
-    })
-    post.content_images.attach(io: File.open('app/assets/images/test1.jpg'), filename: 'test1.jpg')
+    it '投稿を保存できる' do
+      expect(@post).to be_valid
+    end
+  end
 
-    expect(post).to be_valid
+  context '画像が5枚以上の場合' do
+    before do
+      user = User.create!({
+        email: 'test@test.com',
+        username: 'test',
+        password: '123456'
+      })
+      @post = user.posts.build({
+        content: Faker::Lorem.characters(number: 10),
+      })
+      @post.content_images.attach(io: File.open('app/assets/images/test1.jpg'), filename: 'test1.jpg')
+      @post.content_images.attach(io: File.open('app/assets/images/test1.jpg'), filename: 'test1.jpg')
+      @post.content_images.attach(io: File.open('app/assets/images/test1.jpg'), filename: 'test1.jpg')
+      @post.content_images.attach(io: File.open('app/assets/images/test1.jpg'), filename: 'test1.jpg')
+      @post.content_images.attach(io: File.open('app/assets/images/test1.jpg'), filename: 'test1.jpg')
+      @post.save
+    end
+
+    it '投稿を保存できない' do
+      expect(@post.errors.messages[:content_images][0]).to eq('は4枚以内にしてください')
+    end
   end
 end

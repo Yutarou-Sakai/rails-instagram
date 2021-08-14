3e666ff1510bca1efa4ae902a244c2c5f014f0d1
酒井友太郎 <>
2021-08-14 22:32:44 +0900

    一旦、posts controllerのcreateにPOSTリクエストを送ると、ログインしている場合、投稿が保存される

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
     factory :user do
         email { Faker::Internet.email }
-        username { Faker::Name.unique.name }
+        username { Faker::Lorem.characters(number: 5) }
         password { '123456' }
     end
 end 
--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -3,11 +3,35 @@ require 'rails_helper'
 RSpec.describe "Posts", type: :request do
   let!(:user) { create(:user) }
   let!(:posts) { create_list(:post, 5, user: user) }
-
+  
   describe "GET /posts" do
+
     it "200ステータスが返ってくる" do
       get posts_path
       expect(response).to have_http_status(200)
+    end
+  end
+
+
+  describe "POST /posts" do
+    context 'ログインしている場合' do
+      # let!(:images) { fixture_file_upload('app/assets/images/test1.jpg', 'image/jpeg') }
+
+      before do
+        sign_in user
+      end
+
+      it "投稿が保存される" do
+        # image_params = { content_images: [attributes_for(images)] }
+
+        post_params = attributes_for(:post)
+        post posts_path({post: post_params})
+
+        expect(response).to have_http_status(302)
+        expect(Post.last.content).to eq(post_params[:content])
+
+        # post.rbのpresence: trueを外せば通る
+      end
     end
   end
 end
